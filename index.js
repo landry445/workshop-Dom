24c306b5e5cc2a577138bd7bbab220c482be6fa8
Landry Dupont <landrydupont790@gmail.com>
2024-10-15 11:59:46 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const buttons = document.querySelectorAll(".television__button");
+
+for (const button of buttons) {
+    button.addEventListener("click", () => {
+        alert("Clic !");
+    });
+}
+
+const buttonOn = document.querySelector(".television__button--on");
+
+buttonOn.addEventListener("click", () => {
+    const img = document.querySelector(".television__content")
+
+    img.src = "https://picsum.photos/id/237/270/190?grayscale";
+
+    });
+
+const buttonOff = document.querySelector(".television__button--off");
+
+buttonOff.addEventListener("click", () => {
+    const img = document.querySelector(".television__content")
+    
+    img.src = "data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==";
+    
+    });
+
+const channelGrayscale = document.createElement("li");
+channelGrayscale.classList.add("television__channel");
+
+const channels = document.querySelector(".television__channels");
+channels.appendChild(channelGrayscale);
+
+const buttonGrayscale = document.createElement("button");
+buttonGrayscale.type = "button";
+buttonGrayscale.textContent = "T";
+
+channelGrayscale.appendChild(buttonGrayscale);
+
+buttonGrayscale.addEventListener("click", () => {
+    const img = document.querySelector(".television__content");
+
+    const isData = img.src.startsWith("data");
+
+        if (!isData) {
+        const isGrayscale = img.src.endsWith("?grayscale");
+
+        if (isGrayscale) {
+            img.src = img.src.slice(0, -"?grayscale".length);
+        } else {
+            img.src += "?grayscale";
+        }
+    }
+});
+
